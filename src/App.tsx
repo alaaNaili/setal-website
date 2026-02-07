f8e5ed08113c8ead314f80bd5431ca01149e7e9a
Alaa Naili <alaanaili.pro@gmail.com>
2026-02-07 12:52:22 +0100

    fix .gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Help from "./pages/help";
+import HelpPage from "./pages/help";
 
 const queryClient = new QueryClient();
 
@@ -17,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/Help" element={<Help />} />
+          <Route path="/Help" element={<HelpPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
